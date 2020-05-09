63e44a6214e33d6777ce3084fd9e3be83e4ad723
Matt Ammirati <matt.ammirati@gmail.com>
2020-05-09 09:13:38 -1000

    portfolio

---
+++ js/scripts.js
@@ -0,0 +1,129 @@
+
+document.getElementById("fiveguys").onclick = function () {
+    location.href = "https://www.fiveguys.com";
+}
+
+document.getElementById("portillos").onclick = function () {
+    location.href = "https://www.portillos.com";
+}
+
+document.getElementById("chillis").onclick = function () {
+    location.href = "https://www.chillis.com";
+}
+
+document.getElementById("apple").onclick = function () {
+    location.href = "https://www.applebees.com";
+}
+
+document.getElementById("tgif").onclick = function () {
+    location.href = "https://www.tgifridays.com";
+}
+
+document.getElementById("chipotle").onclick = function () {
+    location.href = "https://www.chipotle.com";
+}
+
+document.getElementById("bw3").onclick = function () {
+    location.href = "https://www.buffalowildwings.com";
+}
+document.getElementById("rosebud").onclick = function () {
+    location.href = "https://www.therosebudcafe.com";
+}
+
+document.getElementById("giopza").onclick = function () {
+    location.href = "https://www.giordanos.com";
+}
+
+
+
+
+
+function fiveguysb(x) {
+    x.style.background = "157px";
+    x.style.width = "157px";
+  }
+  
+  function fiveguys(x) {
+    x.style.height = "122px";
+    x.style.width = "122px";
+  }
+
+  function portillosb(x) {
+    x.style.height = "244px";
+    x.style.width = "244px";
+  }
+  
+  function portillos(x) {
+    x.style.height = "225px";
+    x.style.width = "225px";
+  }
+
+  function chillisb(x) {
+    x.style.height = "276px";
+    x.style.width = "226px";
+  }
+  
+  function chillis(x) {
+    x.style.height = "251px";
+    x.style.width = "201px";
+  }
+
+  function appleb(x) {
+    x.style.height = "250px";
+    x.style.width = "250px";
+  }
+  
+  function apple(x) {
+    x.style.height = "225px";
+    x.style.width = "225px";
+  }
+
+  function tgifb(x) {
+    x.style.height = "211px";
+    x.style.width = "296px";
+  }
+  
+  function tgif(x) {
+    x.style.height = "186px";
+    x.style.width = "271px";
+  }
+
+  function chipotleb(x) {
+    x.style.height = "217px";
+    x.style.width = "287px";
+  }
+  
+  function chipotle(x) {
+    x.style.height = "192px";
+    x.style.width = "262px";
+  }
+
+  function bw3b(x) {
+    x.style.height = "250px";
+    x.style.width = "250px";
+  }
+  
+  function bw3(x) {
+    x.style.height = "225px";
+    x.style.width = "225px";
+  }
+
+  function rosebudb(x) {
+    x.style.height = "248px";
+    x.style.width = "251px";
+  }
+  
+  function rosebud(x) {
+    x.style.height = "223px";
+    x.style.width = "226px";
+  }
+
+  function giopzab(x) {
+    x.style.height = "194px";
+    x.style.width = "323px";
+  }
+  
+  function giopza(x) {
+    x.style.height = "169px";
+    x.style.width = "298px";
+  }
