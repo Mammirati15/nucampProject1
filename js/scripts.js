61547f03a88a76b1648dd77fedf01ca8331801b6
Matt Ammirati <matt.ammirati@gmail.com>
2020-05-12 14:30:05 -1000

    compiled and changed images

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+// local Storage- store input
+/*
+const inputEmail= document.getElementById("email");
+localStorage.setItem("email", inputEmail.value);
+
+const inputBusinessName= document.getElementById("businessName");
+localStorage.setItem("businessName", inputText.value);
+
+const inputEin= document.getElementById("ein");
+localStorage.setItem("ein", inputText.value);
+
+const inputTextArea= document.getElementById("textArea");
+localStorage.setItem("textArea", inputText.value);
+
+
+// local Storage - get input
+const storedValueEmail = localStorage.getItem("email");
+const storedValueBusinessName = localStorage.getItem("businessName");
+const storedValueEin = localStorage.getItem("ein");
+const storedValueTextArea = localStorage.getItem("textArea");
+*/
 
 document.getElementById("fiveguys").onclick = function () {
     location.href = "https://www.fiveguys.com";
