ad3b79c4d3bb7f19b45720d08a337621a3788cbf
Matt Ammirati <matt.ammirati@gmail.com>
2020-06-04 18:32:45 -1000

    refactored javascript in ourPortfolio page

--- js/scripts.js
+++ js/scripts.js
@@ -21,130 +21,145 @@ const storedValueTextArea = localStorage.getItem("textArea");
 */
 
 document.getElementById("fiveguys").onclick = function () {
-    location.href = "https://www.fiveguys.com";
-}
+  location.href = "https://www.fiveguys.com";
+};
 
 document.getElementById("portillos").onclick = function () {
-    location.href = "https://www.portillos.com";
-}
+  location.href = "https://www.portillos.com";
+};
 
 document.getElementById("chillis").onclick = function () {
-    location.href = "https://www.chillis.com";
-}
+  location.href = "https://www.chillis.com";
+};
 
 document.getElementById("apple").onclick = function () {
-    location.href = "https://www.applebees.com";
-}
+  location.href = "https://www.applebees.com";
+};
 
 document.getElementById("tgif").onclick = function () {
-    location.href = "https://www.tgifridays.com";
-}
+  location.href = "https://www.tgifridays.com";
+};
 
 document.getElementById("chipotle").onclick = function () {
-    location.href = "https://www.chipotle.com";
-}
+  location.href = "https://www.chipotle.com";
+};
 
 document.getElementById("bw3").onclick = function () {
-    location.href = "https://www.buffalowildwings.com";
-}
+  location.href = "https://www.buffalowildwings.com";
+};
 document.getElementById("rosebud").onclick = function () {
-    location.href = "https://www.therosebudcafe.com";
-}
+  location.href = "https://www.therosebudcafe.com";
+};
 
 document.getElementById("giopza").onclick = function () {
-    location.href = "https://www.giordanos.com";
+  location.href = "https://www.giordanos.com";
+};
+
+const dimensions = {
+  chillis: {
+    normal: {
+      height: 251,
+      width: 201,
+    },
+    focused: {
+      height: 276,
+      width: 226,
+    },
+  },
+  fiveguys: {
+    normal: {
+      height: 122,
+      width: 122,
+    },
+    focused: {
+      height: 222,
+      width: 222,
+    },
+  },
+  portillos: {
+    normal: {
+      height: 225,
+      width: 225,
+    },
+    focused: {
+      height: 244,
+      width: 244,
+    },
+  },
+  apple: {
+    normal: {
+      height: 225,
+      width: 225,
+    },
+    focused: {
+      height: 250,
+      width: 250,
+    },
+  },
+  tgif: {
+    normal: {
+      height: 186,
+      width: 271,
+    },
+    focused: {
+      height: 211,
+      width: 296,
+    },
+  },
+  chipotle: {
+    normal: {
+      height: 192,
+      width: 262,
+    },
+    focused: {
+      height: 217,
+      width: 287,
+    },
+  },
+  bw3: {
+    normal: {
+      height: 225,
+      width: 225,
+    },
+    focused: {
+      height: 250,
+      width: 250,
+    },
+  },
+  rosebud: {
+    normal: {
+      height: 223,
+      width: 226,
+    },
+    focused: {
+      height: 248,
+      width: 251,
+    },
+  },
+  giopza: {
+    normal: {
+      height: 169,
+      width: 298,
+    },
+    focused: {
+      height: 194,
+      width: 323,
+    },
+  },
+};
+
+function onMouseOver(x) {
+  const dims = dimensions[x.id];
+  console.log(dims.focused.height + "px");
+  x.style.height = dims.focused.height + "px";
+  x.style.width = dims.focused.width + "px";
 }
 
+function onMouseOut(x) {
+  const dims = dimensions[x.id];
+  console.log("x", x);
+  console.log(dims);
+  x.style.height = dims.normal.height + "px";
+  x.style.width = dims.normal.width + "px";
+}
 
-
-
-
-function fiveguysb(x) {
-    x.style.background = "225px";
-    x.style.width = "225px";
-  }
-  
-  function fiveguys(x) {
-    x.style.height = "122px";
-    x.style.width = "122px";
-  }
-
-  function portillosb(x) {
-    x.style.height = "244px";
-    x.style.width = "244px";
-  }
-  
-  function portillos(x) {
-    x.style.height = "225px";
-    x.style.width = "225px";
-  }
-
-  function chillisb(x) {
-    x.style.height = "276px";
-    x.style.width = "226px";
-  }
-  
-  function chillis(x) {
-    x.style.height = "251px";
-    x.style.width = "201px";
-  }
-
-  function appleb(x) {
-    x.style.height = "250px";
-    x.style.width = "250px";
-  }
-  
-  function apple(x) {
-    x.style.height = "225px";
-    x.style.width = "225px";
-  }
-
-  function tgifb(x) {
-    x.style.height = "211px";
-    x.style.width = "296px";
-  }
-  
-  function tgif(x) {
-    x.style.height = "186px";
-    x.style.width = "271px";
-  }
-
-  function chipotleb(x) {
-    x.style.height = "217px";
-    x.style.width = "287px";
-  }
-  
-  function chipotle(x) {
-    x.style.height = "192px";
-    x.style.width = "262px";
-  }
-
-  function bw3b(x) {
-    x.style.height = "250px";
-    x.style.width = "250px";
-  }
-  
-  function bw3(x) {
-    x.style.height = "225px";
-    x.style.width = "225px";
-  }
-
-  function rosebudb(x) {
-    x.style.height = "248px";
-    x.style.width = "251px";
-  }
-  
-  function rosebud(x) {
-    x.style.height = "223px";
-    x.style.width = "226px";
-  }
-
-  function giopzab(x) {
-    x.style.height = "194px";
-    x.style.width = "323px";
-  }
-  
-  function giopza(x) {
-    x.style.height = "169px";
-    x.style.width = "298px";
-  }
